1b3f31c39a442eb3c2c9e2f1bbb7c1a5595c281f
CxWatcher <147360379+CxWatcher@users.noreply.github.com>
2024-03-10 01:56:15 +0000

    Update KickEnhancer.user.js

--- KickEnhancer.user.js
+++ KickEnhancer.user.js
@@ -1,21 +1,19 @@
 // ==UserScript==
 // @name         Kick.com Enhancer
 // @namespace    RishiSunak
-// @version      0.1
+// @version      0.2
 // @description  Kick.com Chat Overlay & Auto Unmute/1080p
 // @author       Rishi
 // @match        https://kick.com/*
 // @grant        none
-// @downloadURL https://update.greasyfork.org/scripts/489440/Kickcom%20Enhancer.user.js
-// @updateURL https://update.greasyfork.org/scripts/489440/Kickcom%20Enhancer.meta.js
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     let currentStreamSrc = '';
     let overlayContainer = null;
-
+ 
     function checkStreamChange() {
         const videoElement = document.querySelector('video.vjs-tech');
         if (videoElement) {
@@ -25,7 +23,7 @@
                 if (overlayContainer) {
                     overlayContainer.remove();
                 }
-
+ 
                 setTimeout(() => {
                     customizeVideo();
                     const match = window.location.pathname.match(/^\/(.+)$/);
@@ -37,17 +35,17 @@
             }
         }
     }
-
+ 
     function customizeVideo() {
         const muteControlButton = document.querySelector('.vjs-mute-control');
         if (muteControlButton) {
             const buttonText = muteControlButton.querySelector('.vjs-control-text').textContent.trim().toLowerCase();
-
+ 
             if (buttonText === 'unmute') {
                 simulateClick(muteControlButton);
             }
         }
-
+ 
         // Set video quality to 1080p if available
         const qualityButtons = document.querySelectorAll('.vjs-menu-item-text');
         qualityButtons.forEach(button => {
@@ -55,38 +53,38 @@
                 simulateClick(button);
             }
         });
-
+ 
         console.log('Video customized');
     }
-
+ 
     function createOverlay(streamerName) {
         const videoElement = document.getElementById('mifu-player-check');
         if (!videoElement) {
             return;
         }
-
+ 
         const overlay = document.createElement('div');
         overlay.style.position = 'absolute';
-        overlay.style.bottom = '0';
+        overlay.style.bottom = '32px';
         overlay.style.left = '0';
         overlay.style.width = '100%';
         overlay.style.height = '40vh';
         overlay.style.zIndex = '9999';
         overlay.style.pointerEvents = 'none';
-
+ 
         const iframe = document.createElement('iframe');
         iframe.src = `https://tools.kick.cx/chat?user=${streamerName}&animate=true&badges=true&commands=true&bots=true&textsize=15px`;
         iframe.style.width = '100%';
         iframe.style.height = '100%';
         iframe.style.border = 'none';
-
+ 
         overlay.appendChild(iframe);
         videoElement.parentNode.appendChild(overlay);
-
+ 
         console.log('Overlay created');
         overlayContainer = document.getElementById('overlay-container');
     }
-
+ 
     function simulateClick(element) {
         var event = new MouseEvent('click', {
             bubbles: true,
@@ -95,10 +93,10 @@
         });
         element.dispatchEvent(event);
     }
-
+ 
     setTimeout(() => {
         checkStreamChange();
-
+ 
         setInterval(() => {
             checkStreamChange();
         }, 500);
